d439039cd7758f089d8ef62cbb1bbc8eeaf92456
jamshid <jamshidshamshod0705@gmail.com>
2023-03-03 00:31:57 +0500

    scoping in practice

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// scopes
+function information(birtYear){
+  const age=2023-birtYear
+  console.log(`${firstname} is ${age} years old !`)
+
+  function fullname(){
+    const lastname='Xatamov'
+    console.log(firstname,lastname)
+
+    if(age>=20){
+      var win='You win !'
+      // console.log(win)
+      function a(a,b){
+        console.log(2+9)
+      }
+    }
+    console.log(win)
+    // a()
+  }
+  fullname()
+  // console.log(lastname)
+}
+const firstname = 'Jamshid';
+information(2000)

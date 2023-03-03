bc11139caf34e7b5eec07bd1b97767f9e661c451
jamshid <jamshidshamshod0705@gmail.com>
2023-03-03 23:25:32 +0500

    hoisting and tdz

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
-'use strict';
+// 'use strict';
 
-// scopes
-function information(birtYear){
-  const age=2023-birtYear
-  console.log(`${firstname} is ${age} years old !`)
+//////////// scoping in practice ////////////////
+// function information(birtYear){
+//   const age=2023-birtYear
+//   console.log(`${firstname} is ${age} years old !`)
 
-  function fullname(){
-    const lastname='Xatamov'
-    console.log(firstname,lastname)
+//   function fullname(){
+//     const lastname='Xatamov'
+//     console.log(firstname,lastname)
 
-    if(age>=20){
-      var win='You win !'
-      // console.log(win)
-      function a(a,b){
-        console.log(2+9)
-      }
-    }
-    console.log(win)
-    // a()
-  }
-  fullname()
-  // console.log(lastname)
+//     if(age>=20){
+//       var win='You win !'
+//       // console.log(win)
+//       function a(a,b){
+//         console.log(2+9)
+//       }
+//     }
+//     console.log(win)
+//     // a()
+//   }
+//   fullname()
+//   // console.log(lastname)
+// }
+// const firstname = 'Jamshid';
+// information(2000)
+
+
+/////////////// Hoisting and TDZ in practice ///////////////
+// variables
+// console.log(varExam)
+// console.log(letExam)
+// console.log(constExam)
+
+// varExam='var'
+// let letExam='let'
+// const constExam='const'
+// console.log(varExam)
+// console.log(window)
+
+// functions
+console.log(dec(2,9))
+
+function dec(a,b){
+  return a+b
 }
-const firstname = 'Jamshid';
-information(2000)
+
+ex=function(a,b){
+  return a+b
+}
+
+const arrow=(a,b)=>a+b
+console.log(ex(3, 8));
+console.log(window);
